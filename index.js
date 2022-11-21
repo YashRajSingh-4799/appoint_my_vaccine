8449d1113944d42315fc126a9da3276dccacbbec
Yash Raj Singh <yashrajsingh4799@gmail.com>
2022-11-21 10:52:04 +0530

    express mongoose and other dependense added

---
+++ index.js
@@ -0,0 +1,33 @@
+import  express  from "express";
+import  dotenv  from "dotenv";
+import  mongoose  from "mongoose";
+
+const app = express()
+dotenv.config()
+
+const connect = async () => {
+
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("mongoDB")
+    } catch (error) {
+        throw error
+    }
+};
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongoDB disconnected!")
+})
+
+mongoose.connection.on("connected", ()=>{
+    console.log("mongoDB connected!")
+})
+
+app.get("/", (req,res)=>{
+    res.send("hello first man")
+})
+
+app.listen(8800, ()=>{
+    connect()
+    console.log("connected to my laptop")
+})
